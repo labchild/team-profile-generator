6abb126015faae7076f65c45b78c30a6e31912ec
l bates childs <labchilds@gmail.com>
2022-01-28 19:43:01 -0500

    add Engineer to main logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const { managerQuestions, promptEmployeeData } = require('./src/questions');
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
 
 // function to call app
 const init = (leadQuestions) => {
@@ -17,6 +18,14 @@ const init = (leadQuestions) => {
             const manager = new Manager(answers.managerName, answers.id, answers.email, answers.office);
 
             console.log(JSON.stringify(manager));
+
+            if (answers.employees) {
+                answers.employees.forEach(employee => {
+                    const engineer = new Engineer (employee.name, employee.id, employee.email, employee.github);
+
+                    console.log(JSON.stringify(engineer));
+                })
+            }
         });
 };
 
